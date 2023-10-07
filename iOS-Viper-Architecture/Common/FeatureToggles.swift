d355c5d2d4777bcc4f8933afe6200ce55846dd89
George Kiriy <georgempsnp@gmail.com>
2023-10-07 12:17:15 +0300

    Add sui feature toggle

---
+++ iOS-Viper-Architecture/Common/FeatureToggles.swift
@@ -0,0 +1,13 @@
+//
+//  FeatureToggles.swift
+//  iOS-Viper-Architecture
+//
+//  Created by gnkiriy on 07.10.2023.
+//  Copyright © 2023 Mindorks NextGen Private Limited. All rights reserved.
+//
+
+import Foundation
+
+enum FeatureToggles {
+    static let suiPostList = true
+}

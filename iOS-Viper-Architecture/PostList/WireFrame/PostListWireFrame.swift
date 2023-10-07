ce5a18ca13545b58e68e94219877e009541e5850
George Kiriy <georgempsnp@gmail.com>
2023-10-07 12:34:02 +0300

    SUI wireframe

--- iOS-Viper-Architecture/PostList/WireFrame/PostListWireFrame.swift
+++ iOS-Viper-Architecture/PostList/WireFrame/PostListWireFrame.swift
@@ -7,10 +7,18 @@
 //
 
 import UIKit
+import SwiftUI
 
 class PostListWireFrame: PostListWireFrameProtocol {
-    
     class func createPostListModule(state: PostListState = .init()) -> UIViewController {
+        if FeatureToggles.suiPostList {
+            createPostListModuleSui(state: state)
+        } else {
+            createPostListModuleUIKit(state: state)
+        }
+    }
+
+    class func createPostListModuleUIKit(state: PostListState = .init()) -> UIViewController {
         let navController = mainStoryboard.instantiateViewController(withIdentifier: "PostsNavigationController")
         if let view = navController.childViewControllers.first as? PostListViewController {
             let wireFrame = PostListWireFrame()
@@ -38,7 +46,36 @@ class PostListWireFrame: PostListWireFrameProtocol {
         }
         return UIViewController()
     }
-    
+
+    class func createPostListModuleSui(state: PostListState = .init()) -> UIViewController {
+        let wireFrame = PostListWireFrame()
+
+        let localDataManager: PostListLocalDataManagerInputProtocol = PostListLocalDataManager()
+        let remoteDataManager: PostListRemoteDataManagerInputProtocol = PostListRemoteDataManager()
+
+        let interactor = PostListInteractor(
+            localDatamanager: localDataManager,
+            remoteDatamanager: remoteDataManager
+        )
+
+        let presenter = PostListPresenter(
+            interactor: interactor,
+            wireFrame: wireFrame,
+            state: state
+        )
+
+        let viewModel = PostListViewModel(state: presenter.state, performAction: presenter.perform)
+        let view = PostListView(viewModel: viewModel)
+        let viewController = UIHostingController(rootView: view)
+
+
+        presenter.view = viewModel
+        interactor.presenter = presenter
+        remoteDataManager.remoteRequestHandler = interactor
+
+        return UINavigationController(rootViewController: viewController)
+    }
+
     static var mainStoryboard: UIStoryboard {
         return UIStoryboard(name: "Main", bundle: Bundle.main)
     }
